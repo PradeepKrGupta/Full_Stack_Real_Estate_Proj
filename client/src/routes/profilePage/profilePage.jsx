ba01938434e9c25d9a4e88a428cae25988e0ee6e
Pradeep <pradeeprauniyarofficial@gmail.com>
2024-07-13 01:30:58 +0530

    adding the post, user updation, cloudinary, etc on this commit

--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -2,40 +2,38 @@ import Chat from "../../components/chat/Chat";
 import List from "../../components/list/List";
 import "./profilePage.scss";
 import apiRequest from "../../lib/apiRequest";
-import { useNavigate } from "react-router-dom";
-import { useContext} from "react";
+import { Await, Link, useLoaderData,useNavigate } from "react-router-dom";
+import { Suspense, useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 
 function ProfilePage() {
-  const {updateUser, currentUser} = useContext(AuthContext);
+  const data = useLoaderData();
   const navigate = useNavigate();
-  const handleLogout = async ()=>{
-    try{
+  const { updateUser, currentUser } = useContext(AuthContext);
 
-      // The below code will handle the logout portion when the logout got successful then it navigate to home page and clear the local storage i.e token from the stoarge.
+  const handleLogout = async () => {
+    try {
       await apiRequest.post("/auth/logout");
       updateUser(null);
       navigate("/");
-    }
-    catch(err){
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
   return (
     <div className="profilePage">
       <div className="details">
         <div className="wrapper">
           <div className="title">
             <h1>User Information</h1>
-            <button>Update Profile</button>
+            <Link to="/profile/update">
+              <button>Update Profile</button>
+            </Link>
           </div>
           <div className="info">
             <span>
               Avatar:
-              <img
-                src={currentUser.avatar || "noavatar.jpg"}
-                alt=""
-              />
+              <img src={currentUser.avatar || "noavatar.jpg"} alt="" />
             </span>
             <span>
               Username: <b>{currentUser.username}</b>
@@ -47,18 +45,43 @@ function ProfilePage() {
           </div>
           <div className="title">
             <h1>My List</h1>
-            <button>Create New Post</button>
+            <Link to="/add">
+              <button>Create New Post</button>
+            </Link>
           </div>
-          <List />
+          {/* <Suspense fallback={<p>Loading...</p>}>
+            <Await
+              resolve={data.postResponse}
+              errorElement={<p>Error loading posts!</p>}
+            >
+              {(postResponse) => <List posts={postResponse.data.userPosts} />}
+            </Await>
+          </Suspense> */}
           <div className="title">
             <h1>Saved List</h1>
           </div>
-          <List />
+          <List/>
+          {/* <Suspense fallback={<p>Loading...</p>}>
+            <Await
+              resolve={data.postResponse}
+              errorElement={<p>Error loading posts!</p>}
+            >
+              {(postResponse) => <List posts={postResponse.data.savedPosts} />}
+            </Await>
+          </Suspense> */}
         </div>
       </div>
       <div className="chatContainer">
         <div className="wrapper">
-          <Chat/>
+        <Chat/>
+          {/* <Suspense fallback={<p>Loading...</p>}>
+            <Await
+              resolve={data.chatResponse}
+              errorElement={<p>Error loading chats!</p>}
+            >
+              {(chatResponse) => <Chat chats={chatResponse.data} />}
+            </Await>
+          </Suspense> */}
         </div>
       </div>
     </div>
